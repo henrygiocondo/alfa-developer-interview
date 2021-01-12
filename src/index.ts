ed0b866b529a12025bd433220ee238037df5239f
Eliel <zelimen741@gmail.com>
2021-01-12 12:48:03 -0300

    finalizando api com testes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import * as bodyParser from 'body-parser'
+import bodyParser from 'body-parser'
 import axios from 'axios'
 
 interface ResponsePeople {
@@ -13,10 +13,10 @@ const getPeopleFactory = (peoples: any[]): ResponsePeople[] => peoples.map(el =>
 
 const axiosConfig = {
   headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+    'If-Match': '*'
   }
-};
+}
 
 class App {
 
@@ -24,12 +24,25 @@ class App {
 
   constructor() {
     this.app = express()
+    this.config()
     this.app.get('/is-alive', async (_req, res) => res.send(true))
+
+    this.app.get('/airports', async (req, res) => {
+      try {
+        const { query: { address } } = req
+        if (!address) throw new Error('address é obrigatório na query da requisição.')
+        const { data: { value } } = await axios.get(`${BASE_URL}/Airports?$filter=contains(Location/Address, '${address}')`, axiosConfig)
+        return res.send({ response: value })
+      } catch (error) {
+        return res.status(500).send({ err: true, message: error.message })
+      }
+    })
+
     this.app.get('/people', async (req, res) => {
       try {
         const { query: { username } } = req
         if (!username) throw new Error('username é obrigatório na query da requisição.')
-        const { data: { value } } = await axios.get(`${BASE_URL}/People?$filter=contains(UserName, '${username}')&$select=FirstName, LastName`)
+        const { data: { value } } = await axios.get(`${BASE_URL}/People?$filter=contains(UserName, '${username}')&$select=FirstName, LastName`, axiosConfig)
         const response = getPeopleFactory(value)
         return res.send({ response })
       } catch (error) {
@@ -40,22 +53,28 @@ class App {
     this.app.post('/people', async (req, res) => {
       try {
         const { body } = req
-        const x = await axios.post(`${BASE_URL}/People`, JSON.stringify(body), axiosConfig)
-        console.log(x)
-        return res.send({})
+        const { status } = await axios.post(`${BASE_URL}/People`, body, axiosConfig)
+        if (status !== 201) throw new Error('Erro ao salvar registro')
+        return res.send({ message: 'Sucesso' })
       } catch (error) {
         return res.status(500).send({ err: true, message: error.message })
       }
     })
 
-
-    this.config()
+    this.app.delete('/people/:username', async (req, res) => {
+      try {
+        const { params: { username } } = req
+        await axios.delete(`${BASE_URL}/People('${username}')`, axiosConfig)
+        return res.send({ message: 'Sucesso' })
+      } catch (error) {
+        return res.status(500).send({ err: true, message: error.message })
+      }
+    })
   }
 
   private config(): void {
     this.app.use(bodyParser.json())
     this.app.use(bodyParser.urlencoded({ extended: true }))
-    // this.app.use(express.static('src/public'))
   }
 }
 
